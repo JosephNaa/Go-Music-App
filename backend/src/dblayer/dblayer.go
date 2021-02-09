603018c870a5740dbdad4685b78c40c6f801539b
JosephNaa <js.pekah@gmail.com>
2021-02-09 20:23:33 +0900

    add backend

---
+++ backend/src/dblayer/dblayer.go
@@ -0,0 +1,20 @@
+package dblayer
+
+import (
+	"github.com/PacktPublishing/Hands-On-Full-Stack-Development-with-Go/Chapter06/backend/src/models"
+)
+
+type DBLayer interface {
+	GetAllProducts() ([]models.Product, error)
+	GetPromos() ([]models.Product, error)
+	GetCustomerByName(string, string) (models.Customer, error)
+	GetCustomerByID(int) (models.Customer, error)
+	GetProduct(int) (models.Product, error)
+	AddUser(models.Customer) (models.Customer, error)
+	SignInUser(username, password string) (models.Customer, error)
+	SignOutUserByID(int) error
+	GetCustomerOrdersByID(int) ([]models.Order, error)
+	AddOrder(models.Order) error
+	GetCreditCardID(int) (string, error)
+	SaveCreditCardForCustomer(int, string) error
+}
---
+++ backend/src/rest/handler.go
@@ -0,0 +1,146 @@
+package rest
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"../models"
+	"./dblayer"
+
+	"github.com/gin-gonic/gin"
+)
+
+type HandlerInterface interface {
+	GetProduct(c *gin.Context)
+	GetPromos(c *gin.Context)
+	AddUser(c *gin.Context)
+	SignIn(c *gin.Context)
+	SignOut(c *gin.Context)
+	GetOrders(c *gin.Context)
+	Charge(c *gin.Context)
+}
+
+type Handler struct {
+	db dblayer.DBLayer
+}
+
+func NewHandler() (*Handler, error) {
+	//Handler 객체에 대한 포인터 생성
+	return new(Handler), nil
+}
+
+func (h *Handler) GetProducts(c *gin.Context) {
+	if h.db == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "server database error"})
+		return
+	}
+	products, err := h.db.GetAllProducts()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	fmt.Printf("Found %d products\n", len(products))
+	c.JSON(http.StatusOK, products)
+}
+
+func (h *Handler) GetPromos(c *gin.Context) {
+	if h.db == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "server database error"})
+		return
+	}
+	promos, err := h.db.GetPromos()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, promos)
+}
+
+func (h *Handler) SignIn(c *gin.Context) {
+	if h.db == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "server database error"})
+		return
+	}
+	var customer models.Customer
+	err := c.ShouldBindJSON(&customer)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	customer, err = h.db.SignInUser(customer.Email, customer.Pass)
+	if err != nil {
+		if err == dblayer.ErrINVALIDPASSWORD {
+			c.JSON(http.StatusForbidden, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, customer)
+}
+
+func (h *Handler) AddUser(c *gin.Context) {
+	if h.db == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "server database error"})
+		return
+	}
+	var customer models.Customer
+	err := c.ShouldBindJSON(&customer)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	customer, err = h.db.AddUser(customer)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, customer)
+}
+
+func (h *Handler) SignOut(c *gin.Context) {
+	if h.db == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "server database error"})
+		return
+	}
+	p := c.Param("id")
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = h.db.SignOutUserByID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+}
+
+func (h *Handler) GetOrders(c *gin.Context) {
+	if h.db == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "server database error"})
+		return
+	}
+	p := c.Param("id")
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	orders, err := h.db.GetCustomerOrdersByID(id)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, orders)
+}
+
+func (h *Handler) Charge(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+}
